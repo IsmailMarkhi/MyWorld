05e364549a9461a3dd5f662b561af7f24a60c67d
ISMAIL <ismailmarkhi17@gmail.com>
2025-12-17 01:13:54 +0100

    Add Google Search Console verification file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
           className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight mb-6"
         >
           Simple websites 
-          <br className="hidden sm:flex" />
+          <br className="hidden sm:block ml-2" />
           built with care
         </h1>
 
