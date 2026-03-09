7a5c00b1e60377c47c657180500e89288dab5465
ISMAIL <ismailmarkhi17@gmail.com>
2026-03-09 22:11:55 +0000

    ui optimize

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,22 @@ export default function App() {
 
       <main
         className="
+              min-h-screen
+    pt-16
+
+    bg-gradient-to-b
+    from-white
+    via-indigo-50
+    to-white
+
+    dark:from-zinc-900
+    dark:via-zinc-900
+    dark:to-black
+
+    text-slate-900
+    dark:text-slate-100
+        "
+      >{/*
           min-h-screen
           pt-16
           bg-gradient-to-b
@@ -22,8 +38,25 @@ export default function App() {
           via-indigo-50
           to-white
           text-slate-900
-        "
-      >
+        <main
+  className="
+    min-h-screen
+    pt-16
+
+    bg-gradient-to-b
+    from-white
+    via-indigo-50
+    to-white
+
+    dark:from-zinc-900
+    dark:via-zinc-900
+    dark:to-black
+
+    text-zinc-900
+    dark:text-zinc-100
+  "
+>
+      */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
